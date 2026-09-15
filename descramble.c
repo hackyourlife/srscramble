5685dac700ec1447e138a23530d7e091e0dd57e4
hackyourlife <root@localhost>
2026-09-16 01:01:21 +0200

    Optimize descrambling function and add support for JD-990/SC88/JV880

--- descramble.c
+++ descramble.c
@@ -8,84 +8,62 @@ typedef uint8_t		u8;
 typedef uint16_t	u16;
 typedef uint32_t	u32;
 
-u8 descramble_data8(u8 word)
+static inline u8 descramble_data8(const u8 word)
 {
 	return    (word & 0x0002) << 6
 		| (word & 0x0008) << 3
 		| (word & 0x0040) >> 1
 		| (word & 0x0080) >> 3
-		| (word & 0x0020) >> 2
-		| (word & 0x0010) >> 2
-		| (word & 0x0001) << 1
-		| (word & 0x0004) >> 2;
+		| (word & 0x0034) >> 2
+		| (word & 0x0001) << 1;
 }
 
-u16 descramble_data16(u16 word)
+static inline u16 descramble_data16(const u16 word)
 {
-	return    (word & 0x0002) << 6
-		| (word & 0x0008) << 3
-		| (word & 0x0040) >> 1
-		| (word & 0x0080) >> 3
-		| (word & 0x0020) >> 2
-		| (word & 0x0010) >> 2
-		| (word & 0x0001) << 1
-		| (word & 0x0004) >> 2
-		| (word & 0x0200) << 6
-		| (word & 0x0800) << 3
-		| (word & 0x4000) >> 1
-		| (word & 0x8000) >> 3
-		| (word & 0x2000) >> 2
-		| (word & 0x1000) >> 2
-		| (word & 0x0100) << 1
-		| (word & 0x0400) >> 2;
+	return    (word & 0x0202) << 6
+		| (word & 0x0808) << 3
+		| (word & 0x4040) >> 1
+		| (word & 0x8080) >> 3
+		| (word & 0x3434) >> 2
+		| (word & 0x0101) << 1;
 }
 
-u32 descramble_addr(u32 addr, int width)
+static inline u32 descramble_addr8(const u32 addr)
 {
-	if(width == 8) {
-		return    (addr & 0x00000001) << 1
-			| (addr & 0x00000002) << 3
-			| (addr & 0x00000004) >> 2
-			| (addr & 0x00000008) >> 1
-			| (addr & 0x00000010) >> 1
-			| (addr & 0x00000020) << 10
-			| (addr & 0x00000040) << 4
-			| (addr & 0x00000080) << 10
-			| (addr & 0x00000100) << 6
-			| (addr & 0x00000200) >> 4
-			| (addr & 0x00000400) >> 3
-			| (addr & 0x00000800) << 1
-			| (addr & 0x00001000) << 4
-			| (addr & 0x00002000) >> 7
-			| (addr & 0x00004000) << 4
-			| (addr & 0x00008000) >> 4
-			| (addr & 0x00010000) >> 3
-			| (addr & 0x00020000) >> 8
-			| (addr & 0x00040000) >> 10
-			| (addr & 0xFFF80000);
-	} else {
-		return    (addr & 0x00000002) << 3
-			| (addr & 0x00000010) >> 3
-			| (addr & 0x00000020) << 3
-			| (addr & 0x00000040) << 6
-			| (addr & 0x00000080) >> 1
-			| (addr & 0x00000100) << 5
-			| (addr & 0x00000200) << 2
-			| (addr & 0x00000400) >> 1
-			| (addr & 0x00000800) << 5
-			| (addr & 0x00001000) >> 5
-			| (addr & 0x00002000) >> 8
-			| (addr & 0x00008000) << 2
-			| (addr & 0x00010000) >> 6
-			| (addr & 0x00020000) >> 2
-			| (addr & 0xFFFC400D);
-	}
+	return    (addr & 0x00000801) << 1
+		| (addr & 0x00000002) << 3
+		| (addr & 0x00000004) >> 2
+		| (addr & 0x00000018) >> 1
+		| (addr & 0x000000A0) << 10
+		| (addr & 0x00005040) << 4
+		| (addr & 0x00000100) << 6
+		| (addr & 0x00008200) >> 4
+		| (addr & 0x00010400) >> 3
+		| (addr & 0x00002000) >> 7
+		| (addr & 0x00020000) >> 8
+		| (addr & 0x00040000) >> 10
+		| (addr & 0xFFF80000);
+}
+
+static inline u32 descramble_addr16(const u32 addr)
+{
+	return    (addr & 0x00000022) << 3
+		| (addr & 0x00000010) >> 3
+		| (addr & 0x00000040) << 6
+		| (addr & 0x00000480) >> 1
+		| (addr & 0x00000900) << 5
+		| (addr & 0x00008200) << 2
+		| (addr & 0x00001000) >> 5
+		| (addr & 0x00002000) >> 8
+		| (addr & 0x00010000) >> 6
+		| (addr & 0x00020000) >> 2
+		| (addr & 0xFFFC400D);
 }
 
 int main(int argc, char** argv)
 {
 	if(argc != 3) {
-		printf("Usage: %s scrambled.bin descrambled.bin", *argv);
+		printf("Usage: %s scrambled.bin descrambled.bin\n", *argv);
 		return 1;
 	}
 
@@ -114,9 +92,14 @@ int main(int argc, char** argv)
 	// figure out ROM type
 	if(strncmp(buf, "Roland", 6)) {
 		if(strncmp(buf, "JP-800", 6)) {
-			printf("Invalid ROM: %c%c%c%c%c%c\n", buf[0], buf[1],
-					buf[2], buf[3], buf[4], buf[5]);
-			return 1;
+			if(strncmp(buf, "RK-10E", 6)) {
+				printf("Invalid ROM: %c%c%c%c%c%c\n", buf[0], buf[1],
+						buf[2], buf[3], buf[4], buf[5]);
+				return 1;
+			} else {
+				width = 8;
+				type = "JD-990";
+			}
 		} else {
 			width = 8;
 			type = "JP-800";
@@ -127,9 +110,18 @@ int main(int argc, char** argv)
 	} else if(!strncmp(&buf[0xC], "O\xB0X", 3)) {
 		width = 16;
 		type = "SRX";
+	} else if(!strncmp(&buf[0x7], "JV80", 4)) {
+		width = 8;
+		type = "JV880";
+	} else if(!strncmp(&buf[0x7], "XP-GS", 5)) {
+		width = 16;
+		type = "SC88";
 	} else {
-		printf("Unknown ROM type: %c%c%c\n", buf[0xC], buf[0xD],
-				buf[0xE]);
+		printf("Unknown ROM type: ");
+		for(int i = 0; i < 16; i++) {
+			printf("%c", buf[i]);
+		}
+		printf("\n");
 		return 1;
 	}
 
@@ -144,14 +136,14 @@ int main(int argc, char** argv)
 		u16* buf16 = (u16*) buf;
 		u16* outbuf16 = (u16*) outbuf;
 		for(size_t i = 0; i < fsize; i += 2) {
-			u32 addr = descramble_addr(i, width);
+			u32 addr = descramble_addr16(i);
 			u16 tmp = descramble_data16(buf16[i >> 1]);
 			outbuf16[addr >> 1] = tmp;
 		}
 	} else {
 		// no optimization for 8bit ROMs, because A[0] is scrambled too
 		for(size_t i = 0; i < fsize; i++) {
-			u32 addr = descramble_addr(i, width);
+			u32 addr = descramble_addr8(i);
 			u16 tmp = descramble_data8(buf[i]);
 			outbuf[addr] = tmp;
 		}
@@ -160,7 +152,7 @@ int main(int argc, char** argv)
 
 	// print ROM info
 	if(width == 8) {
-		// SR-JV80 / JP-800
+		// SR-JV80 / JV880 / JP-800 / JD-990
 		printf("ROM ID:   ");
 		for(int i = 0x20; i < 0x26; i++) {
 			printf("%c", outbuf[i]);
@@ -172,7 +164,7 @@ int main(int argc, char** argv)
 		}
 		printf("\n");
 	} else if(width == 16) {
-		// SRX
+		// SRX / SC88
 		printf("ROM ID:   ");
 		for(int i = 0x20; i < 0x30; i++) {
 			printf("%c", outbuf[i]);
